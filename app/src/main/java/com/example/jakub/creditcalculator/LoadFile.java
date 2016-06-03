67584e86e4eeb56dea0df37e880c52aadc890486
jakub-k <kuba_94_k@wp.pl>
2016-06-03 04:35:22 +0200

    added 'save changes' button

--- app/src/main/java/com/example/jakub/creditcalculator/LoadFile.java
+++ app/src/main/java/com/example/jakub/creditcalculator/LoadFile.java
@@ -1,8 +1,15 @@
 package com.example.jakub.creditcalculator;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class LoadFile extends AppCompatActivity {
 
@@ -18,4 +25,18 @@ public class LoadFile extends AppCompatActivity {
         data12 = (EditText)findViewById(R.id.editText4);
         data12.setText(extras.getString("DATA12"));
     }
+
+    public void saveChanges(View view) {
+        String fileName = "credit";
+        File file = new File(Environment.getExternalStorageDirectory(), fileName);
+        try {
+            FileOutputStream fo = new FileOutputStream(file);
+            fo.write(data12.getText().toString().getBytes());
+            fo.close();
+            Toast.makeText(getApplicationContext(),"Data changed in internal storage/credit", Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
